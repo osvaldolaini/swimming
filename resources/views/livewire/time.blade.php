383f4037c89746e1a880043bb4462e84fe53df26
LaiGuz <laiguz@hotmail.com>
2023-09-15 15:57:26 -0300

    master

--- resources/views/livewire/time.blade.php
+++ resources/views/livewire/time.blade.php
@@ -180,7 +180,7 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option value="tomada">Tomada</option>
                             <option value="prova">Prova</option>
-                            <option value="parcial">Parcial</option>
+                            {{-- <option value="parcial">Parcial</option> --}}
                         </select>
                         @error('type_time')
                             <span class="error">{{ $message }}</span>
@@ -316,7 +316,7 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option value="tomada">Tomada</option>
                             <option value="prova">Prova</option>
-                            <option value="parcial">Parcial</option>
+                            {{-- <option value="parcial">Parcial</option> --}}
                         </select>
                         @error('type_time')
                             <span class="error">{{ $message }}</span>
